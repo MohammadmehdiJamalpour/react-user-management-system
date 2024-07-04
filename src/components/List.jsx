e807b8f92162b927b6fd53b157cc38ba47bba60a
Mohammad Mehdi Jamalpour <mohammadmehdi.jamalpour28@gmail.com>
2024-07-04 18:34:10 +0330

    List Component static UI added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,40 @@
 function List() {
   return (
-    <div className="bg-gray-300 p-4  rounded-xl">
-      <div className="p-3 grid border rounded-xl bg-gray-100">
-        <div className=""></div>
+    <div className="w-full bg-gray-300  p-2 rounded-xl shadow-md mt-3">
+      <div className="p-4 bg-gray-300 ">
+        <div className="border border-opacity-30 rounded-xl border-gray-400 overflow-hidden shadow-sm">
+          <div className="grid grid-cols-5 text-gray-700 bg-gray-200 justify-center items-center ">
+            <p className="font-bold p-2 justify-self-center">ردیف</p>
+            <p className="font-bold p-2 justify-self-center">نام</p>
+            <p className="font-bold p-2 justify-self-center">نام خانوادگی</p>
+            <p className="font-bold p-2 justify-self-center">کد ملی</p>
+            <p className="font-bold p-2 justify-self-center">عملیات</p>
+          </div>
+
+          <div className="grid grid-cols-5 bg-gray-100 items-center border-b border-opacity-30 border-gray-400">
+            <p className=" p-2 justify-self-center rounded-lg">1</p>
+            <p className=" p-2 justify-self-center rounded-lg">محمد</p>
+            <p className=" p-2 justify-self-center rounded-lg">حسینی</p>
+            <p className=" p-2 justify-self-center rounded-lg">002344234</p>
+            <p className=" p-2 justify-self-center rounded-lg">عملیات 1</p>
+          </div>
+
+          <div className="grid grid-cols-5 bg-gray-100 items-center border-opacity-30 border-b border-gray-400">
+            <p className=" p-2 justify-self-center rounded-lg">1</p>
+            <p className=" p-2 justify-self-center rounded-lg">محمد</p>
+            <p className=" p-2 justify-self-center rounded-lg">حسینی</p>
+            <p className=" p-2 justify-self-center rounded-lg">002344234</p>
+            <p className=" p-2 justify-self-center rounded-lg">عملیات 1</p>
+          </div>
+
+          <div className="grid grid-cols-5 bg-gray-100 items-center border-opacity-30  border-gray-400">
+            <p className=" p-2 justify-self-center rounded-lg">1</p>
+            <p className=" p-2 justify-self-center rounded-lg">محمد</p>
+            <p className=" p-2 justify-self-center rounded-lg">حسینی</p>
+            <p className=" p-2 justify-self-center rounded-lg">002344234</p>
+            <p className=" p-2 justify-self-center rounded-lg">عملیات 1</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 function SearchBar() {
   return (
-    <div className=" bg-gray-300 flex flex-col p-4  justify-center rounded-xl">
+    <div className=" bg-gray-300 flex flex-col p-4  justify-center rounded-xl shadow-md">
       <h3 className="font-bold text-lg text-gray-700">جستجو</h3>
-      <div className="flex bg-gray-100 p-3 mt-4  rounded-xl justify-between ">
+      <div className="flex bg-gray-100 shadow-sm p-3 mt-4  rounded-xl justify-between ">
         <div className="flex gap-4    ">
           <div className="flex gap-1 items-center ">
             <p className="text-gray-600">نام :</p>
@@ -10,8 +10,8 @@ function SearchBar() {
               type="text"
               className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
               name=""
-              placeholder="مثال : علی ..."
               id=""
+           
             />
           </div>
           <div className="flex gap-1 items-center">
@@ -20,7 +20,7 @@ function SearchBar() {
               type="text"
               className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
               name=""
-              placeholder="مثال : محمدی ..."
+             
               id=""
             />
           </div>
@@ -28,18 +28,18 @@ function SearchBar() {
             <p className="text-gray-700">کد ملی :</p>
             <input
               type="text"
+              
               className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
               name=""
-              placeholder="مثال : 002 ..."
               id=""
             />
           </div>
         </div>
         <div className="flex gap-2 justify-between ml-3">
-          <button className="px-4 py-2  bg-gray-400 rounded-xl text-white">
+          <button className="px-4 py-2 shadow-sm  bg-gray-400 rounded-xl text-white">
             جستجو
           </button>
-          <button className="px-4 py-2  bg-gray-400 rounded-xl text-white">
+          <button className="px-4 py-2 shadow-sm  bg-gray-400 rounded-xl text-white">
             افزودن
           </button>
         </div>
