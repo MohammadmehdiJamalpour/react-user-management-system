89f7633d81c58dfae64ad90da83d4bd0431f9805
Mohammad Mehdi Jamalpour <mohammadmehdi.jamalpour28@gmail.com>
2024-07-05 16:47:27 +0330

    Search and add user functions added

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,37 @@
 import SearchBar from "./SearchBar";
 import List from "./List";
+import { useState } from "react";
+import usersData from "../users.json";
 
 function Container() {
+  const [users, setUsers] = useState(usersData);
+  const [allUsers] = useState(usersData); // Keep a copy of all users
+  const handleAddUser = (newUser) => {
+    setUsers((users) => [...users, newUser]);
+  };
+
+  const handleDeleteUser = (id) => {
+    setUsers((users) => users.filter((u) => u.id !== id));
+  };
+
+  const handleSearch = (searchCriteria) => {
+    const filteredUsers = allUsers.filter((user) => {
+      return (
+        (!searchCriteria.name || user.name.includes(searchCriteria.name)) &&
+        (!searchCriteria.familyName ||
+          user.familyName.includes(searchCriteria.familyName)) &&
+        (!searchCriteria.idCardNumber ||
+          user.idCardNumber.includes(searchCriteria.idCardNumber))
+      );
+    });
+
+    setUsers(filteredUsers);
+  };
+
   return (
     <div className="container gap-2 flex flex-col mx-auto px-4 pt-4 max-w-5xl ">
-      <SearchBar />
-      <List />
+      <SearchBar onAddUser={handleAddUser} onSearch={handleSearch} />
+      <List users={users} onDelete={handleDeleteUser} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,40 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar({ onAddUser, onSearch }) {
+  const [name, setName] = useState("");
+  const [familyName, setfamilyName] = useState("");
+  const [idCardNumber, setidCardNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    // Checking data that must be valid
+    if (!name || !familyName || !idCardNumber) {
+      alert("لطفا اطلاعات کامل را وارد کنید");
+      setName("");
+      setfamilyName("");
+      setidCardNumber("");
+      return null;
+    }
+    e.preventDefault();
+    // Creating new user
+    const newUser = {
+      name,
+      familyName,
+      idCardNumber,
+      id: Date.now(),
+    };
+    onAddUser(newUser);
+    setName("");
+    setfamilyName("");
+    setidCardNumber("");
+  };
+
+  // Searching users
+  const handleSearch = (e) => {
+    e.preventDefault();
+    onSearch({ name, familyName, idCardNumber });
+
+  };
+
   return (
     <div className=" bg-gray-300 flex flex-col p-4  justify-center rounded-xl shadow-md">
       <h3 className="font-bold text-lg text-gray-700">جستجو</h3>
@@ -8,19 +44,25 @@ function SearchBar() {
             <p className="text-gray-600">نام :</p>
             <input
               type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
               className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
               name=""
               id=""
-           
             />
           </div>
           <div className="flex gap-1 items-center">
             <p className="text-gray-700">نام خانوادگی :</p>
             <input
               type="text"
+              value={familyName}
+              onChange={(e) => {
+                setfamilyName(e.target.value);
+              }}
               className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
               name=""
-             
               id=""
             />
           </div>
@@ -28,7 +70,10 @@ function SearchBar() {
             <p className="text-gray-700">کد ملی :</p>
             <input
               type="text"
-              
+              value={idCardNumber}
+              onChange={(e) => {
+                setidCardNumber(e.target.value);
+              }}
               className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
               name=""
               id=""
@@ -36,10 +81,16 @@ function SearchBar() {
           </div>
         </div>
         <div className="flex gap-2 justify-between ml-3">
-          <button className="px-4 py-2 shadow-sm  bg-gray-400 rounded-xl text-white">
+          <button
+            onClick={handleSearch}
+            className="px-4 py-2 shadow-md  bg-gray-400 rounded-xl text-white"
+          >
             جستجو
           </button>
-          <button className="px-4 py-2 shadow-sm  bg-gray-400 rounded-xl text-white">
+          <button
+            onClick={handleSubmit}
+            className="px-4 py-2 shadow-md  bg-gray-400 rounded-xl text-white"
+          >
             افزودن
           </button>
         </div>
