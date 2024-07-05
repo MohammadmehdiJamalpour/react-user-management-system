c965b6ee875ed5f37abd1c239f4ef88e22130255
Mohammad Mehdi Jamalpour <mohammadmehdi.jamalpour28@gmail.com>
2024-07-05 23:21:16 +0330

    search button has been deleted and search is based on change of input + add user is pop up modal now

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+
+function AddUser({ isOpen, onClose, onSave }) {
+  const [newUser, setNewUser] = useState({ name: "", familyName: "", idCardNumber: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = () => {
+    if (!newUser.name || !newUser.familyName || !newUser.idCardNumber) {
+      alert("لطفا اطلاعات صحیح را وارد کنید");
+      return;
+    }
+    onSave({ ...newUser, id: Date.now() });
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75 z-50">
+      <div className="bg-white rounded-lg shadow-xl w-full max-w-md p-4">
+        <div className="flex justify-between items-center border-b pb-2">
+          <h2 className="text-xl font-bold">افزودن کاربر جدید</h2>
+          <button
+            className="text-gray-500 hover:text-gray-700"
+            onClick={onClose}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7 rounded-full inline-block p-1 bg-gray-400 text-white hover:bg-gray-600 transition duration-300"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="mt-4 space-y-4 ">
+          <div className="flex items-center">
+            <label htmlFor="nameInput" className="w-1/3 text-right">
+              نام:
+            </label>
+            <input
+              type="text"
+              id="nameInput"
+              name="name"
+              value={newUser.name}
+              onChange={handleInputChange}
+              className="bg-inherit w-2/3 border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
+            />
+          </div>
+          <div className="flex items-center">
+            <label htmlFor="familyNameInput" className="w-1/3 text-right">
+              نام خانوادگی:
+            </label>
+            <input
+              type="text"
+              id="familyNameInput"
+              name="familyName"
+              value={newUser.familyName}
+              onChange={handleInputChange}
+              className="bg-inherit w-2/3 border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
+            />
+          </div>
+          <div className="flex items-center">
+            <label htmlFor="idCardNumberInput" className="w-1/3 text-right">
+              کد ملی:
+            </label>
+            <input
+              type="text"
+              id="idCardNumberInput"
+              name="idCardNumber"
+              value={newUser.idCardNumber}
+              onChange={handleInputChange}
+              className="bg-inherit w-2/3 border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
+            />
+          </div>
+          <div className="mt-4 flex justify-end">
+            <button
+              className="bg-gray-500 hover:bg-gray-700 text-white py-1.5 px-3 rounded-xl transition duration-300"
+              onClick={handleSave}
+            >
+              افزودن
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddUser;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,45 +1,26 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import AddUserModal from "./AddUser"; // Adjust the import path as needed
 
 function SearchBar({ onAddUser, onSearch }) {
   const [name, setName] = useState("");
-  const [familyName, setfamilyName] = useState("");
-  const [idCardNumber, setidCardNumber] = useState("");
+  const [familyName, setFamilyName] = useState("");
+  const [idCardNumber, setIdCardNumber] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleSubmit = (e) => {
-    // Checking data that must be valid
-    if (!name || !familyName || !idCardNumber) {
-      alert("لطفا اطلاعات کامل را وارد کنید");
-      setName("");
-      setfamilyName("");
-      setidCardNumber("");
-      return null;
-    }
-    e.preventDefault();
-    // Creating new user
-    const newUser = {
-      name,
-      familyName,
-      idCardNumber,
-      id: Date.now(),
-    };
-    onAddUser(newUser);
-    setName("");
-    setfamilyName("");
-    setidCardNumber("");
-  };
-
-  // Searching users
-  const handleSearch = (e) => {
-    e.preventDefault();
+  useEffect(() => {
     onSearch({ name, familyName, idCardNumber });
+  }, [name, familyName, idCardNumber]);
+
+  const handleAddUser = (user) => {
+    onAddUser(user);
   };
 
   return (
-    <div className=" bg-gray-300 flex flex-col p-4  justify-center rounded-xl shadow-md">
+    <div className="bg-gray-300 flex flex-col p-4 justify-center rounded-xl shadow-md">
       <h3 className="font-bold text-lg text-gray-700">جستجو</h3>
-      <div className="flex bg-gray-100 shadow-sm p-3 mt-4  rounded-xl justify-between ">
-        <div className="flex gap-4    ">
-          <div className="flex gap-1 items-center ">
+      <div className="flex bg-gray-100 shadow-sm p-3 mt-4 rounded-xl justify-between">
+        <div className="flex gap-4">
+          <div className="flex gap-1 items-center">
             <label htmlFor="name" className="text-gray-600">
               نام :
             </label>
@@ -47,58 +28,52 @@ function SearchBar({ onAddUser, onSearch }) {
               type="text"
               id="name"
               value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
-              name=""
-             
+              onChange={(e) => setName(e.target.value)}
+              placeholder="جستجو ..."
+              className="bg-inherit border  placeholder:text-gray-300 rounded-lg p-1.5  text-xs focus:outline-none focus:border-2 focus:border-gray-300"
             />
           </div>
           <div className="flex gap-1 items-center">
-            <label htmlFor="familyName" className="text-gray-700">نام خانوادگی :</label>
+            <label htmlFor="familyName" className="text-gray-700">
+              نام خانوادگی :
+            </label>
             <input
               type="text"
-              
               value={familyName}
-              onChange={(e) => {
-                setfamilyName(e.target.value);
-              }}
-              className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
-              name=""
+              onChange={(e) => setFamilyName(e.target.value)}
+              placeholder="جستجو ..."
+              className="bg-inherit border placeholder:text-gray-300 rounded-lg p-1.5 placeholder: text-xs focus:outline-none focus:border-2 focus:border-gray-300"
               id="familyName"
             />
           </div>
           <div className="flex gap-1 items-center">
-            <label htmlFor="idCardNumber" className="text-gray-700">کد ملی :</label>
+            <label htmlFor="idCardNumber" className="text-gray-700">
+              کد ملی :
+            </label>
             <input
               type="text"
-              
               value={idCardNumber}
-              onChange={(e) => {
-                setidCardNumber(e.target.value);
-              }}
-              className="bg-inherit border rounded-lg p-1 text-sm focus:outline-none focus:border-2 focus:border-gray-300"
-              name=""
+              onChange={(e) => setIdCardNumber(e.target.value)}
+              placeholder="جستجو ..."
+              className="bg-inherit border placeholder:text-gray-300 rounded-lg p-1.5 placeholder: text-xs focus:outline-none focus:border-2 focus:border-gray-300"
               id="idCardNumber"
             />
           </div>
         </div>
         <div className="flex gap-2 justify-between ml-3">
           <button
-            onClick={handleSearch}
-            className="px-4 py-2 shadow-md  bg-gray-400 rounded-xl text-white hover:bg-gray-600 transition duration-300"
-          >
-            جستجو
-          </button>
-          <button
-            onClick={handleSubmit}
-            className="px-4 py-2 shadow-md  bg-gray-400 rounded-xl text-white hover:bg-gray-600 transition duration-300"
+            onClick={() => setIsModalOpen(true)}
+            className="px-4 py-2 shadow-md bg-gray-400 rounded-xl text-white hover:bg-gray-600 transition duration-300"
           >
             افزودن
           </button>
         </div>
       </div>
+      <AddUserModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSave={handleAddUser}
+      />
     </div>
   );
 }
